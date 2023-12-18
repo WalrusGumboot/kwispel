ed63496708c2993aac370dca388994d0c3184164
Simeon Duwel <tameimpalafan123@gmail.com>
2023-12-18 17:03:00 +0100

    betere vragen

--- kwispel-client/src/lib/Kwis.ts
+++ kwispel-client/src/lib/Kwis.ts
@@ -18,8 +18,11 @@ export const PUNTEN_PER_STEM = 100;
 
 export const standaardKwis: Kwis = {
     vragen: [
-        { soort: "tekst", tekst: "De salamanders van Vaes be like: 'Senior, commilitones, laat ons drinken op {}!'" },
-        { soort: "tekst", tekst: "Uw moeder is zo {} dat ze {}." }
+        { soort: "tekst", tekst: "Eigenlijk schudt Vaes altijd met z'n krijtje omdat {}." },
+        { soort: "tekst", tekst: "Als we {} dan zal Leen gegarandeerd op tijd klaarkomen." },
+        { soort: "tekst", tekst: "Arno Kuijlaars' standaard pickup line is: '{}'" },
+        { soort: "tekst", tekst: "Ik gebruik mijn Giancoli meestal voor {}." },
+        { soort: "tekst", tekst: "De Rony Keppens 3000 text-to-speech bot mist één belangrijke functie: {}." },
     ],
     huidigeVraagIdx: 0,
     fase: "nogNietBegonnen"
