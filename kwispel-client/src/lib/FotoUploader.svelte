8e4af38ca60aab4000fa7cf86034bdd74ba1d836
Simeon Duwel <tameimpalafan123@gmail.com>
2023-12-19 11:48:46 +0100

    fotos groter dan een megabyte werken nu ook

--- kwispel-client/src/lib/FotoUploader.svelte
+++ kwispel-client/src/lib/FotoUploader.svelte
@@ -8,7 +8,6 @@
 
     let geüploadeBestanden: any;
     let geüploadeFoto: string;
-    
 
     let fotoInput: HTMLInputElement;
 
@@ -16,20 +15,13 @@
         const reader = new FileReader();
         reader.readAsDataURL(foto);
         reader.onload = (e) => {
-            geüploadeFoto = e.target?.result?.toString()!
-        }
+            geüploadeFoto = e.target?.result?.toString()!;
+        };
     }
 </script>
 
-<div class="p-6 bg-white min-w-max flex flex-col gap-4 justify-stretch">
+<div class="p-6 bg-white max-w-screen flex flex-col gap-4 justify-stretch">
     <h1 class="text-2xl">Upload een foto!</h1>
-    {#if geüploadeFoto}
-        <img alt="geüploade foto" class="w-max" src={geüploadeFoto}/>
-    {:else}
-        <div class="w-max h-16 min-w-max min-h-max bg-slate-200 rounded-sm flex flex-col items-center align-center justify-center">
-            <p class="text-slate-800 text-lg">Geen foto gekozen.</p>
-        </div>
-    {/if}
     <input
         class="hidden"
         bind:files={geüploadeBestanden}
@@ -42,11 +34,26 @@
         class={knopStijl.get(spelerRichting)}
         on:click={() => {
             fotoInput.click();
-        }}>
-            {geüploadeFoto ? "Kies een andere foto" : "Kies een foto"}
-            {#if geüploadeFoto}
-                <button class={knopStijl.get(spelerRichting)} on:click={() => {dataOntvangenCallback(geüploadeFoto)}}>verstuur antwoord</button>
-            {/if}
-        </button
+        }}
     >
+        {geüploadeFoto ? "Kies een andere foto" : "Kies een foto"}
+    </button>
+    {#if geüploadeFoto}
+        <button
+            class={knopStijl.get(spelerRichting)}
+            on:click={() => {
+                dataOntvangenCallback(geüploadeFoto);
+            }}>verstuur antwoord</button
+        >
+    {/if}
+
+    {#if geüploadeFoto}
+        <img alt="geüploade foto" class="w-max" src={geüploadeFoto} />
+    {:else}
+        <div
+            class="p-6 min-w-max bg-slate-200 rounded-sm flex flex-col items-center align-center justify-center"
+        >
+            <p class="text-slate-800 text-lg">Geen foto gekozen.</p>
+        </div>
+    {/if}
 </div>
